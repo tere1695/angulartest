11e09905652610c3ef84e972c59adea5445ef6e8
Teresita <teresita.ambrosio@net-effect.com>
2017-06-16 16:57:49 -0300

    commit input email validacion

--- public_html/lorac/js/app.js
+++ public_html/lorac/js/app.js
@@ -1,1 +1,1 @@
-var app = angular.module('lorac', []);
+var app = angular.module('myApp', []);
--- public_html/lorac/js/controllers/MainCtrl.js
+++ public_html/lorac/js/controllers/MainCtrl.js
@@ -1,3 +1,17 @@
-  app.controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.formData = {};
-  }]);
+/* global app */
+var map = [];
+
+app.controller('myController', ["$scope",function($scope){
+    $scope.emailPattern = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
+    
+    
+    $scope.addVals = function() 
+    {
+        map.push({ 
+            email: $scope.email,
+            nombre: "talcosa"
+        });
+        console.log(map);
+    };
+}]);
+
